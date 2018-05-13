888f446e31a8bcbc9593036e02f098d3b49f72dd
Cole <2bigshot@iMac.local>
2018-05-12 20:16:25 -0600

    Next upload

--- scripts/firstTopic.js
+++ scripts/firstTopic.js
@@ -1,8 +1,10 @@
 function fillLoop() {
     var howMany = document.getElementById('times').value;
-    var number = document.getElementById('repeat').value;
+
+    var repeat = document.getElementById('repeat').value;
+
     for (i = 0; i < howMany; i++) {
-        document.getElementById('outputText').innerHTML += number;
+        document.getElementById('outputText').innerHTML += repeat;
     } 
 }
 
@@ -12,24 +14,51 @@ function clearOutputDiv() {
 
 function changeColor() {
     var color = document.getElementById('colorChange').value;
+
     document.getElementById('outputText').style.color = color;
 }
 
 function changeFont() {
     var fontChange = document.getElementById('fontChange').value;
+
     document.getElementById('outputText').style.fontFamily = fontChange;
 }
 
-function createObject()
+function saveWork()
 {
-    var storageName = document.getElementById('storageName').value;
-    var savedObject = {color:document.getElementById('colorChange').value, font:document.getElementById('fontChange').value, textBlock:document.getElementById('outputText').innerHTML};
+    var storageName = document.getElementById('saveName').value;
+
+    var savedObject = {color:document.getElementById('outputText').style.color, font:document.getElementById('outputText').style.fontFamily, textBlock:document.getElementById('outputText').innerHTML};
+
     var ObjectJSON = JSON.stringify(savedObject); 
-    store(ObjectJSON);
-    localStorage.setItem("textBox", objectJSON);
+
+    localStorage.setItem("storage" + storageName, ObjectJSON);
 }
 
-function load(name)
+function loadWork()
 {
-    
+    var loadName = document.getElementById('loadName').value
+
+    var loadedJSON = localStorage.getItem("storage" + loadName);
+
+    var loadedObject = JSON.parse(loadedJSON);
+
+    document.getElementById('outputText').innerHTML = loadedObject.textBlock;
+
+    document.getElementById('outputText').style.color = loadedObject.color;
+    document.getElementById('outputText').style.fontFamily = loadedObject.font;
+
+
+}
+
+function test()
+{
+    var wow = "asdfasdfasdf";
+    localStorage.setItem("ugh", wow);
+}
+
+function testy()
+{
+    var amazing = localStorage.getItem("ugh");
+    document.getElementById('outputText').innerHTML = amazing;
 }
