9db4eb160304b81c4f4a5b41d9cda3be9062158e
Cole <2bigshot@iMac.local>
2018-07-05 19:19:02 -0600

    update

--- final/scripts.js
+++ final/scripts.js
@@ -2,7 +2,31 @@ var animation = document.getElementById("flexwrappermain");
 
 animation.addEventListener("animationend", removeEnterAnimation, false);
 
-getLocation();
+if(localStorage.locationArray)
+{
+    var locations = JSON.parse(localStorage.locationArray);
+}
+else
+{
+    var locations = new Array();
+
+    /*for(var i = 0; i < 100; i++)
+    {
+        locations[i] = "-1";
+    }*/
+    localStorage.locationArray = JSON.stringify(locations);
+}
+
+if((localStorage.lon || localStorage.lat))
+{
+    loadingScreenActivate();
+    buildCachedLocationURLS();
+}
+
+else
+{
+    getLocation();
+}
 
 var weatherURL;
 var forecastURL;
@@ -31,9 +55,9 @@ function loadWeather(weatherURL){
 
         document.getElementById("description").innerHTML = capitalizeFirstLetter(weatherInfo.weather[0].description);
 
-        document.getElementById("temp").innerHTML = weatherInfo.main.temp + "&#176;F"
+        document.getElementById("temp").innerHTML = weatherInfo.main.temp.toFixed(2) + "&#176;F"
 
-        document.getElementById("highlow").innerHTML = weatherInfo.main.temp_max + "&#176;F / " + weatherInfo.main.temp_min + "&#176;F";
+        document.getElementById("highlow").innerHTML = weatherInfo.main.temp_max.toFixed(2) + "&#176;F / " + weatherInfo.main.temp_min.toFixed(2) + "&#176;F";
 
         document.getElementById("icon").src = "icons/" + weatherInfo.weather[0].icon + ".png";
 
@@ -60,13 +84,13 @@ function loadForecast(forecastURL){
             //document.getElementById("desc" + i).innerHTML = capitalizeFirstLetter(forecastInfo.list[i - 1].weather["0"].description);
 
             //document.getElementById("temp" + i).innerHTML = forecastInfo.list[i - 1].main.temp + "&#176;F";
-            
-            var increment = (((i - 1) * 8) + 7);
 
-            document.getElementById("high" + i).innerHTML = parseInt(forecastInfo.list[increment].main.temp_max);
+            var increment = (((i - 1) * 8) + 6);
+
+            document.getElementById("high" + i).innerHTML = forecastInfo.list[increment].main.temp_max.toFixed(2);
 
 
-            document.getElementById("low" + i).innerHTML = " / " + parseInt(forecastInfo.list[increment].main.temp_min);
+            document.getElementById("low" + i).innerHTML = " / " + forecastInfo.list[increment].main.temp_min.toFixed(2);
 
 
             document.getElementById("icon" + i).src = "icons/" + forecastInfo.list[increment].weather["0"].icon + ".png";
@@ -97,6 +121,22 @@ function buildLocationURLS(position) {
 
     var lon = position.coords.longitude;
     var lat = position.coords.latitude;
+
+    localStorage.lon = lon;
+    localStorage.lat = lat;
+    localStorage.time = new Date().getTime() / 1000;
+
+    weatherURL = "https://api.openweathermap.org/data/2.5/weather?" + "lat=" + lat + "&lon=" + lon + "&units=imperial&APPID=0e08266a16752f6b3e85f662e36178a6"
+
+    forecastURL = "https://api.openweathermap.org/data/2.5/forecast?" + "lat=" + lat + "&lon=" + lon + "&units=imperial&APPID=0e08266a16752f6b3e85f662e36178a6"
+
+    loadWeatherAndForecast(weatherURL, forecastURL);
+}
+
+function buildCachedLocationURLS() {
+
+    var lon = localStorage.lon;
+    var lat = localStorage.lat;
 
     weatherURL = "https://api.openweathermap.org/data/2.5/weather?" + "lat=" + lat + "&lon=" + lon + "&units=imperial&APPID=0e08266a16752f6b3e85f662e36178a6"
 
@@ -139,4 +179,28 @@ function triggerEnterAnimation()
 function removeEnterAnimation()
 {
     document.getElementById("flexwrappermain").classList.remove("bounceIn")
+}
+
+function updateLocation()
+{
+    getLocation();
+}
+
+function addLocation()
+{
+    var locationBox = document.getElementById("locationBox");
+
+    if(locationBox.value != "")
+    {
+        locations.push(locationBox.value);
+    }
+
+    console.log(locations.length);
+
+    locationBox.value = "";
+
+    localStorage.locationArray = JSON.stringify(locations);
+
+
+
 }
